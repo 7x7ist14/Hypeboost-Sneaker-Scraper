d42316408536d0cd5e5d4315412a8e48672ef685
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-03-07 21:43:35 +0100

    Update discord_embed.py

--- discord_embed.py
+++ discord_embed.py
@@ -15,12 +15,14 @@ if not TOKEN:
 if not CHANNEL_NAME:
     raise ValueError("The Channel-name was not included in the config.py file")
 
+
 hypeboost_preise = Scraper_Main.product_search
 product_url = Scraper_Main.product_url
 product_title = Scraper_Main.product_title
 product_picture = Scraper_Main.product_picture
 stockx_url = Scraper_Main.stockx_url
 restocks_url = Scraper_Main.restocks_url
+sneakit_url = Scraper_Main.sneakit_product_url
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
@@ -38,15 +40,17 @@ async def on_message(message):
 
   if message.channel.name == CHANNEL_NAME:
     if message.content.startswith(f'$scrape'): #(Prefix "§") keyword for scraping
+      await message.channel.send("Scraping...")
 
       if f'$scrape' in message_content:
           SKU = message_content.replace('$scrape ', '') # everything after prefix is the search (SKU)
-          text = hypeboost_preise(SKU) #SKU used for scraping
+          hypeboost_sizes = hypeboost_preise(SKU) #SKU used for scraping
           product_url_output = product_url(SKU)
           product_title_output = product_title(product_url_output)
           product_picture_output = product_picture(product_url_output)
           stockx_url_output = stockx_url(SKU)
           restocks_url_output = restocks_url(SKU)
+          sneakit_url_output = sneakit_url(SKU)
           embed = discord.Embed(
             title=product_title_output,
             url=product_url_output,
@@ -62,17 +66,16 @@ async def on_message(message):
           )
           embed.add_field(
             name="Prices:",
-            value=text
+            value=hypeboost_sizes
           )
           embed.add_field(
-            name='Open Product on:',
-            value=f"[StockX     ]({stockx_url_output})" f"[Restocks     ]({restocks_url_output})",
+            name="Open Product on:",
+            value=f"[[StockX]]({stockx_url_output})      " f"[[Sneakit]]({sneakit_url_output})      " f"[[Restocks]]({restocks_url_output})      " f"[[Hypeboost]]({product_url_output})      ",
             inline=False
           )
           embed.set_footer(
             text="Developed by Jakob.AIO"
           )
-
           await message.channel.send(embed=embed) #sends sizes in discord chat
           print('Scraping Successful!')
 
